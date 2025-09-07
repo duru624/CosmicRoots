55cb55b61460771308e2751307f6a7ce8ada0883
DA <da@duru-MacBook-Air.local>
2025-09-07 17:46:35 +0300

    Clean project: eski App.jsx taşındı, proje çalışır hale getirildi

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { IonApp, IonPage, IonContent, IonButton, IonHeader, IonTitle, IonToolbar } from '@ionic/react'
+
+function App() {
+  return (
+    <IonApp>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>CosmicRoots 🌱</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent className='ion-padding'>
+          <h2>Hoşgeldiniz!!</h2>
+          <IonButton expand="block" color="dark">
+            Hadi Başlayalım
+          </IonButton>
+        </IonContent>
+      </IonPage>
+    </IonApp>
+  )
+}
+
+export default App
